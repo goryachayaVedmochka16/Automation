7981d3ac3509966e6aecf5242ea8c604d25b4d22
goryachayaVedmochka16 <lena.1994@mail.ru>
2016-12-28 23:01:43 +0300

    Added new steps, 4 more tests

--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -1,8 +1,11 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 
 import steps.Steps;
 
@@ -10,13 +13,34 @@ public class Tests {
 
 	private Steps steps;
 	private static final String URL = "http://www.4chan.org/";
-	private static final String LINKTEXT = "Quests";
-	private static final String PAGE_TITLE = "/qst/ - Quests - 4chan";
+	private static final String LOGO_URL = "//www.4chan.org/";
+	private static final String LINKTEXT = "Rules";
+	private static final String THEME = "Quests";
+	private static final String VALID_QUERY = "hello";
+	private static final String INVALID_QUERY = "HEH&853e";
+	private static final String ERROR_MESSAGE = "Nothing Found";
+	private static final String TOOLTIP = "Home";
+	private static final String PAGE_TITLE = "Rules - 4chan";
+	private static final String[] LINKS = { "About", "Feedback", "Legal", "Contact" };
 
 	@Before
 	public void init() {
 		steps = new Steps();
 		steps.initDriver();
+	}
+
+	@Test
+	public void testVerifyCorrectCopyright() {
+		steps.openPage(URL);
+		String[] actualLinks = steps.getCopyrightLinks().toArray(new String[0]);
+		assertArrayEquals(LINKS, actualLinks);
+	}
+
+	@Test
+	public void testVerifyHomeToolip() {
+		steps.openPage(URL);
+		WebElement link = steps.getLinkByUrl(LOGO_URL);
+		assertEquals(TOOLTIP, steps.getTooltip(link));
 	}
 
 	@Test
@@ -27,9 +51,26 @@ public class Tests {
 		assertEquals(PAGE_TITLE, steps.getPageTitle());
 	}
 
+	@Test
+	public void testSearchByValidText() {
+		steps.openPage(URL);
+		steps.clickOnLink(THEME);
+		steps.closePopup();
+		steps.search(VALID_QUERY);
+		assertTrue(steps.isTextPresent(VALID_QUERY));
+	}
+
+	@Test
+	public void testSearchByInvalidText() {
+		steps.openPage(URL);
+		steps.clickOnLink(THEME);
+		steps.closePopup();
+		steps.search(INVALID_QUERY);
+		assertTrue(steps.isTextPresent(ERROR_MESSAGE));
+	}
+
 	@After
 	public void close() {
 		steps.closeDriver();
 	}
-
 }
--- src/main/java/steps/Steps.java
+++ src/main/java/steps/Steps.java
@@ -1,21 +1,25 @@
 package steps;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Steps {
+	private static final String LINK_URL = "a[href='%s']";
+	private static final String ANY_TEXT_LOCATOR = "//*[text()='%s']";
 	private WebDriver driver = null;
 
 	public void initDriver() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver_win32/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 
 	public void closeDriver() {
@@ -24,6 +28,11 @@ public class Steps {
 
 	public void openPage(String url) {
 		driver.get(url);
+	}
+
+	public WebElement getLinkByUrl(String url) {
+		String locator = String.format(LINK_URL, url);
+		return driver.findElement(By.cssSelector(locator));
 	}
 
 	public void clickOnLink(String linkText) {
@@ -40,5 +49,29 @@ public class Steps {
 
 	public String getPageTitle() {
 		return driver.getTitle();
+	}
+
+	public ArrayList<String> getCopyrightLinks() {
+		List<WebElement> links = driver.findElements(By.cssSelector("div[id='copyright'] a"));
+		ArrayList<String> linkTexts = new ArrayList<String>();
+		for (WebElement link : links) {
+			linkTexts.add(link.getText());
+		}
+		return linkTexts;
+	}
+
+	public String getTooltip(WebElement el) {
+		return el.getAttribute("title");
+	}
+
+	public void search(String query) {
+		WebElement search = driver.findElement(By.id("search-box"));
+		search.sendKeys(query);
+		search.sendKeys(Keys.ENTER);
+	}
+
+	public boolean isTextPresent(String text) {
+		List<WebElement> results = driver.findElements(By.xpath(String.format(ANY_TEXT_LOCATOR, text)));
+		return !results.isEmpty();
 	}
 }
